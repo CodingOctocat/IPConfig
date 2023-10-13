243f368253f88235d4d6148ef81abeb699ac2bcb
CodingNinja <CodingNinja@outlook.com>
2023-10-13 11:29:04 +0800

    Add DeferredContent class to improve startup performance

---
+++ IPConfig/Controls/DeferredContent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace IPConfig.Controls;
+
+/// <summary>
+/// <see href="https://stackoverflow.com/a/26543731/4380178">Deferred loading of XAML</see>
+/// </summary>
+public class DeferredContent : ContentPresenter
+{
+    public static readonly DependencyProperty DeferredContentTemplateProperty =
+        DependencyProperty.Register("DeferredContentTemplate",
+        typeof(DataTemplate), typeof(DeferredContent), null);
+
+    public DataTemplate DeferredContentTemplate
+    {
+        get => (DataTemplate)GetValue(DeferredContentTemplateProperty);
+        set => SetValue(DeferredContentTemplateProperty, value);
+    }
+
+    public event EventHandler<RoutedEventArgs>? DeferredContentLoaded;
+
+    public DeferredContent()
+    {
+        Loaded += HandleLoaded;
+    }
+
+    public void ShowDeferredContent()
+    {
+        if (DeferredContentTemplate is not null)
+        {
+            Content = DeferredContentTemplate.LoadContent();
+            RaiseDeferredContentLoaded();
+        }
+    }
+
+    private void HandleLoaded(object sender, RoutedEventArgs e)
+    {
+        Loaded -= HandleLoaded;
+        Dispatcher.BeginInvoke(ShowDeferredContent);
+    }
+
+    private void RaiseDeferredContentLoaded()
+    {
+        DeferredContentLoaded?.Invoke(this, new RoutedEventArgs());
+    }
+}
